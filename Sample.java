a3d0c81879a7a913f705a56e06b8cc972240e271
Renuka Allada <renuka@Sureedus-MacBook-Air.local>
2025-03-09 10:52:03 -0500

    Comleted 3 problems

--- Sample.java
+++ Sample.java
@@ -33,4 +33,48 @@ class Sample{
             return max;
         }
     }
+
+/*******************PROBLEM-3*****************/
+    //TC:0(N)
+//SC:0(1)
+    class Solution {
+        public void nextPermutation(int[] nums) {
+            if(nums==null || nums.length==0){
+                return;
+            }
+            int n=nums.length;
+            int i=n-2;
+
+            //1.Find an index at which element is smaller than its right neighbour
+            while(i>=0 && nums[i]>=nums[i+1]){
+                i--;
+            }
+            //2.Find an element to the right which is just greater than j
+            if(i!=-1){
+                int j=n-1;
+                while(j>i && nums[j]<=nums[i]){
+                    j--;
+                }
+                swap(nums,i,j);
+            }
+            //3.Reverse from i to the end
+            reverse(nums,i+1,n-1);
+
+        }
+        private void swap(int[] nums,int i,int j){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+        }
+
+        private void reverse(int[] nums,int left,int right){
+            while(left<right){
+                int temp=nums[left];
+                nums[left]=nums[right];
+                nums[right]=temp;
+                left++;
+                right--;
+            }
+        }
+    }
 }
